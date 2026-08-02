54156f9253d02b5d566b5bd740cb7f80deba8093
Kurshid Alam <kurshidalam7715@gmail.com>
2026-08-03 01:19:20 +0530

    Switch from native bcrypt to pure JS bcryptjs to eliminate node-pre-gyp binary issues on Render

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,5 +1,5 @@
 import { User } from "../models/user.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import sendMail from "../middlewares/sendmails.js";
 import TryCatch from "../middlewares/tryCatch.js";
